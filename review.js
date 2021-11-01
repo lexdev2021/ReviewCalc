f948806befbda1c377c679d2ceff2e951a0323de
lexdev2021 <prisedmassa@yandex.by>
2021-11-01 19:42:26 +0300

    Калькулятор для ревью

--- review.js
+++ review.js
@@ -2,7 +2,7 @@ let firstNumber = +prompt('Введите первое число', '');
 let iden = prompt('Введите идентификатор действия', '');
 let secondNumber = +prompt('Введите второе число', '');
 function Calc(firstNumber, iden, secondNumber) {
-    if((firstNumber == '') || (secondNumber == '') || (iden== '') || isNaN(firstNumber)  || isNaN(firstNumber)) {
+    if((firstNumber == '') || (secondNumber == '') || (iden== '') || isNaN(firstNumber)  || isNaN(secondNumber)) {
         return alert('Error');
     }
     else if(iden == '+') {
@@ -16,6 +16,12 @@ function Calc(firstNumber, iden, secondNumber) {
     }
     else if(iden == '/') {
         return alert(firstNumber / secondNumber);
+    }
+    else if(iden == '%') {
+        return alert(firstNumber % secondNumber);
+    }
+    else if(iden == '**') {
+        return alert(firstNumber ** secondNumber);
     }
 }
 Calc(firstNumber, iden, secondNumber);
